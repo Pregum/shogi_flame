5800e5d25b7da92b66c451ec3451a0d2ff70f2f5
Pregum <pregum.y@gmail.com>
2022-01-13 01:36:31 +0900

    update: 将棋盤を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 import 'package:flutter/material.dart';
@@ -33,5 +34,12 @@ class IsometricMapGame extends FlameGame {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    final sprite = await loadSprite('normal_shogi_tile.png');
+    add(SpriteComponent(
+      sprite: sprite,
+      position: Vector2(size.x / 2, size.y / 2),
+      size: sprite.srcSize * 2,
+      anchor: Anchor.center,
+    ));
   }
 }
