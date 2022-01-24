d6b4fdc3c7467bece20f862a7c3adeaba79a6af5
Pregum <pregum.y@gmail.com>
2022-01-25 07:55:30 +0900

    update: 各行のspriteクラスの実装
    * piece_factory.dartのcreateSpritePieceメソッド内の他の駒の実装
    * 各駒のボタンの配置

---
+++ lib/widget/piece/bishop/sprite_bishop.dart
@@ -0,0 +1,19 @@
+import 'package:flame/components.dart';
+import 'package:shogi_game/widget/piece/interface/i_piece.dart';
+import 'package:shogi_game/widget/piece/model/piece_type.dart';
+
+class SpriteBishop extends SpriteComponent implements IPiece {
+  SpriteBishop(Sprite sprite) : super(sprite: sprite);
+
+  static Future<SpriteBishop> initialize() async {
+    final sprite = await Sprite.load('gold_general.png');
+    return SpriteBishop(sprite);
+  }
+
+  @override
+  PieceType pieceType = PieceType.GoldGeneral;
+
+  @override
+  // TODO: implement nextMovableSqure
+  bool get nextMovableSqure => throw UnimplementedError();
+}
