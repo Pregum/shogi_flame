4428518ff134688f1709aaedb1f85dd5dbe34176
Pregum <pregum.y@gmail.com>
2022-05-16 02:30:08 +0900

    add: firebase関連のプラグインも追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:dashbook/dashbook.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shogi_game/stories/utils.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final dashbook = Dashbook(
     theme: ThemeData.dark(),
     title: 'shogi flame example',
