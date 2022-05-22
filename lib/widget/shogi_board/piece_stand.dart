3ab5917866c0199b78f8408907787b54113f1e67
Pregum <pregum.y@gmail.com>
2022-05-23 01:36:49 +0900

    wip: 駒の配置を実装

--- lib/widget/shogi_board/piece_stand.dart
+++ lib/widget/shogi_board/piece_stand.dart
@@ -1,7 +1,94 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 
+import '../piece/interface/i_piece.dart';
+import '../piece/model/piece_type.dart';
+import '../piece/model/player_type.dart';
+import '../piece/util/piece_factory.dart';
+
 /// 駒台
-class PieceStand extends SpriteComponent {
-  PieceStand(double size, Sprite sprite)
-      : super(size: Vector2.all(size), sprite: sprite);
+class PieceStand extends PositionComponent {
+  /// 駒台を所持しているplayer typeです。
+  final PlayerType playerType;
+
+  /// 駒を保持するholderです。
+  Map<PieceType, List<IPiece>> _pieceHolder = Map<PieceType, List<IPiece>>();
+
+  late ShapeComponent _standComponent;
+
+  static final double defaultPieceSize = 64.0;
+
+  /// ctor
+  PieceStand({required this.playerType, super.size});
+
+  @override
+  Future<void>? onLoad() async {
+    await super.onLoad();
+    await _preparePieceStand();
+  }
+
+  /// 駒台の生成を行います。
+  Future<void> _preparePieceStand() async {
+    // 行うこと
+    // holderの初期化
+    _pieceHolder = Map<PieceType, List<IPiece>>();
+    // 駒台の描画
+    add(
+      _standComponent = RectangleComponent(
+        size: super.size,
+        paint: Paint()..color = Color.fromARGB(255, 255, 165, 20),
+      ),
+    );
+  }
+
+  void pushPiece(IPiece piece) {
+    if (_pieceHolder.containsKey(piece.pieceType)) {
+      _pieceHolder[piece.pieceType]?.add(piece);
+    } else {
+      _pieceHolder[piece.pieceType] = <IPiece>[piece];
+    }
+    _updatePieceStandLayout();
+  }
+
+  /// 駒を排出する。
+  /// 排出したコマはリストから削除されます。
+  IPiece? popPiece(PieceType gavenPieceType) {
+    if (!_pieceHolder.containsKey(gavenPieceType) ||
+        (_pieceHolder[gavenPieceType]?.isEmpty ?? true)) {
+      return null;
+    }
+
+    // 存在する場合は、１つ目のpieceを渡す
+    final last = _pieceHolder[gavenPieceType]?.removeLast();
+    return last;
+  }
+
+  Future<void> _updatePieceStandLayout() async {
+    // ここでレイアウトの更新を行います。
+    int index = 0;
+    for (var entry in _pieceHolder.entries) {
+      if (entry.value.isEmpty) {
+        continue;
+      }
+
+      final piece = await PieceFactory.createSpritePiece(
+          entry.value.first.pieceType, defaultPieceSize,
+          playerType: playerType)
+        ?..topLeftPosition = Vector2(index * defaultPieceSize, 0);
+
+      if (piece == null) {
+        continue;
+      }
+      piece.add(
+        TextComponent(
+          text: entry.value.length.toString(),
+          position: Vector2(56.0, 8.0),
+        ),
+      );
+
+      await add(piece);
+      index++;
+    }
+  }
 }
